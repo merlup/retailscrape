6fb4e4ca7dafabfa119ca71852066884b29f9f40
C3PO <CPO@c3pos-imac.attlocal.net>
2016-10-28 00:05:48 -0700

    update

--- app/workers/scraper_worker.rb
+++ app/workers/scraper_worker.rb
@@ -6,20 +6,20 @@ class ScraperWorker
 
   def perform(selector,model_type,model_id,model_section,current_user_id)
     owner_id = current_user_id
-    @next_page = 1
-    @item_count = 0
-    doc = Nokogiri::HTML(open("http://www1.bloomingdales.com/shop/#{model_section}/#{model_type}/Pageindex/#{@next_page}?id=#{model_id}"))
+    next_page = 1
+    item_count = 0
+    doc = Nokogiri::HTML(open("http://www1.bloomingdales.com/shop/#{model_section}/#{model_type}/Pageindex/#{next_page}?id=#{model_id}"))
      # This query is prefomed to create four needed variables.
       # Current_page is the First Page we initial query. 
       # Total Items is a string ie "100 products" this string then gets parsed for all Numbers in the string and then set as Total_number.
       # Number of Pages is needed to know how many pages we should have parsed
       doc.css(selector).each do |result|
         @first_page = (result.css(".currentPage").text[-1]).to_i
-        total_items = result.css('#productCount').text.strip
-        @total_number = total_items.scan(/\d/).join('')
+        @total_items = result.css('#productCount').text.strip
+        @total_number = @total_items.scan(/\d/).join('')
         @number_of_pages = (@total_number.to_f / 90).round
       end 
-    while @item_count <= @total_number.to_f
+    while item_count <= @total_number.to_f
      doc.css(selector).each do |result|
         result.css(".thumbnailItem").each do |product|
         	
@@ -34,12 +34,10 @@ class ScraperWorker
               @product.sale_price = product.at_css(".priceSale").text.strip
             end
           @product.save
-          @item_count = @item_count + 1
-          @next_page = @next_page + 1
+          @item_count = item_count + 1
+          next_page = next_page + 1
         end
-         
       end
-      p "Pringint", Product.all.length, current_user.products.length
     end
   end
 

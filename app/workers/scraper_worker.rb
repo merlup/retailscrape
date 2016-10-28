e721e8e501be4b893a653f059976c6336a2e1cd1
C3PO <CPO@c3pos-imac.attlocal.net>
2016-10-28 00:23:44 -0700

    update

--- app/workers/scraper_worker.rb
+++ app/workers/scraper_worker.rb
@@ -6,10 +6,10 @@ class ScraperWorker
 
   def perform(selector,model_type,model_id,model_section,current_user_id)
     owner_id = current_user_id
-    next_page = 1
-    item_count = 0
-    doc = Nokogiri::HTML(open("http://www1.bloomingdales.com/shop/#{model_section}/#{model_type}/Pageindex/#{next_page}?id=#{model_id}"))
-     # This query is prefomed to create four needed variables.
+
+    doc = Nokogiri::HTML(open("http://www1.bloomingdales.com/shop/#{model_section}/#{model_type}/Pageindex/1?id=#{model_id}"))
+    
+      # This query is prefomed to create four needed variables.
       # Current_page is the First Page we initial query. 
       # Total Items is a string ie "100 products" this string then gets parsed for all Numbers in the string and then set as Total_number.
       # Number of Pages is needed to know how many pages we should have parsed
@@ -19,11 +19,16 @@ class ScraperWorker
         @total_number = total_items.scan(/\d/).join('')
         @number_of_pages = (@total_number.to_f / 90).round
       end 
+
+    next_page = 1
+    item_count = 0
+    doc = Nokogiri::HTML(open("http://www1.bloomingdales.com/shop/#{model_section}/#{model_type}/Pageindex/#{next_page}?id=#{model_id}"))
+   
     while item_count <= @total_number.to_f
      doc.css(selector).each do |result|
         result.css(".thumbnailItem").each do |product|
         	
-          @product = Product.create
+          @product = Product.new
           @product.user_id =  owner_id
           @product.description = product.at_css("#prodName").text
           @product.brand = product.at_css("#brandName").text
@@ -34,7 +39,7 @@ class ScraperWorker
               @product.sale_price = product.at_css(".priceSale").text.strip
             end
           @product.save
-          @item_count = item_count + 1
+          item_count = item_count + 1
           next_page = next_page + 1
         end
       end
